7721ec8fb1dd7abea0ab03274ad04a4196f46bf0
SHRIJASINHA2405 <shrijasinha216@gmail.com>
2026-09-05 00:13:32 +0530

    SESSION 2 - ASSIGNMENT PROBLEM NO. 1 - ATM PIN LENGTH VALIDATOR

---
+++ src/main/java/session_two_stringoperationsandperformance/assignment_programs/ATMPinLengthValidatorDemo.java
@@ -0,0 +1,29 @@
+package session_two_stringoperationsandperformance.assignment_programs;
+import java.util.Scanner;
+
+public class ATMPinLengthValidatorDemo {
+
+    // Method to check PIN length
+    public static void checkPinLength(String pin) {
+
+        if (pin.length() != 4) {
+            System.out.println(
+                    "Invalid PIN — must be exactly 4 digits."
+            );
+        } else {
+            System.out.println("PIN length OK.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter PIN: ");
+        String pin = scanner.nextLine();
+
+        checkPinLength(pin);
+
+        scanner.close();
+    }
+}
